03f2e9340d72a92c1039813fad4731083d2ef219
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-05-23 17:25:08 +0300

    modify db.json

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-    baseURL: "https://json-server-vercel-theta-ten.vercel.app/api/",
+    baseURL: "https://json-server-vercel-theta-ten.vercel.app/api",
 })
 
 export default axiosInstance;  
