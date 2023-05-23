b8e30dc510c6dd0d5391e7a96cfcdf4f0613787d
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-05-23 16:29:02 +0300

    deploy json server to vercel then configure it with todo

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://json-server-five-beryl.vercel.app",
 })
 
 export default axiosInstance;  
